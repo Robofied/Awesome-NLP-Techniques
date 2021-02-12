01abc3990828c7b5064ab9040570f6503dd9e380
mansi75 <51503378+mansi75@users.noreply.github.com>
2021-02-12 16:15:50 +0530

    Update stemming.py

--- src/Lemmatization and Stemming/stemming.py
+++ src/Lemmatization and Stemming/stemming.py
@@ -1,1 +1,14 @@
+# importing required modules 
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+   
+p = PorterStemmer() 
 
+#The sentence to be stemmed
+sentence = "Robofied is a comprehensive Artificial Intelligence platform based in Gurugram,Haryana working towards democratizing safe artificial intelligence towards a common goal of Singularity. At Robofied, we are doing research in speech, natural language, and machine learning. We develop open-source solutions for developers which empowers them so that they can make better products for the world. We educate people about Artificial Intelligence, its scope and impact via resources and tutorials."
+
+#Tokenizing the sentence
+words = word_tokenize(sentence) 
+   
+for w in words: 
+    print(w, " : ", p.stem(w)) 
