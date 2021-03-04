5415f58969982a095815595b8491b16d4146bcff
mansi75 <51503378+mansi75@users.noreply.github.com>
2021-03-04 12:38:10 +0530

    Delete src directory

--- src/lemmatization/TextBlob.py
+++
@@ -1,9 +0,0 @@
-#install textblob using "pip install textblob" and then import it
-from textblob import TextBlob, Word
-
-#Lemmatize the sentence
-sentence = "Robofied is a comprehensive Artificial Intelligence platform based in Gurugram,Haryana working towards democratizing safe artificial intelligence towards a common goal of Singularity. At Robofied, we are doing research in speech, natural language, and machine learning. We develop open-source solutions for developers which empowers them so that they can make better products for the world. We educate people about Artificial Intelligence, its scope and impact via resources and tutorials."
-
-sent = TextBlob(sentence)
-
-" ". join([w.lemmatize() for w in sent.words])
